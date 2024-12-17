94514d7e64bb57b56d4347fd8253a15bed65d443
Bima-Js-Rinz <49645185+bimaadam@users.noreply.github.com>
2024-12-17 11:03:19 +0700

    No Logo In Navbar

--- components/navbar.tsx
+++ components/navbar.tsx
@@ -53,7 +53,7 @@ export const Navbar = () => {
       <NavbarContent className="basis-1/5 sm:basis-full" justify="start">
         <NavbarBrand className="gap-3 max-w-fit">
           <NextLink className="flex justify-start items-center gap-1" href="/">
-            <Logo />
+            {/* <Logo /> */}
             <p className="font-bold text-inherit">Media Downloader</p>
           </NextLink>
         </NavbarBrand>
