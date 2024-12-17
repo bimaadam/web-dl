5c18e716b6253cfb994a3e2ea556a0539a9decc3
Bima-Js-Rinz <49645185+bimaadam@users.noreply.github.com>
2024-12-17 11:09:04 +0700

    Add SEO

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -6,7 +6,12 @@ import { fontSans } from "@/config/fonts";
 export default function Document() {
   return (
     <Html lang="en">
-      <Head />
+      <Head>
+        <meta name="description" content="Media Downloader - Unduh konten dari Instagram, TikTok, Pinterest, dan lainnya dengan mudah dan cepat!" />
+        <meta name="keywords" content="Media Downloader, Instagram Downloader, TikTok Downloader, Pinterest Downloader, Download Video, Save Media" />
+        <meta name="author" content="Bima Adam" />
+        <link rel="icon" href="/favicon.ico" />
+      </Head>
       <body
         className={clsx(
           "min-h-screen bg-background font-sans antialiased",
